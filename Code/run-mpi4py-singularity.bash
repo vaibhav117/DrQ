4a3f2079dbf0311f3556a643d64dfd3bbe359402
vaibhav117 <vaibhavmathur117@gmail.com>
2021-10-06 05:43:11 -0400

    Adding slurm files

---
+++ Code/run-mpi4py-singularity.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+args=''
+for i in "$@"; do 
+    i="${i//\\/\\\\}"
+    args="$args \"${i//\"/\\\"}\""
+done
+
+if [ "$args" == "" ]; then args="/bin/bash"; fi
+
+if [[ "$(hostname -s)" =~ ^g[r,v] ]]; then nv="--nv"; fi
+
+source /scratch/work/public/singularity/greene-ib-slurm-bind.sh
+
+singularity exec $nv \
+	    --overlay /scratch/vm2134/projects/deep_rl/overlay-50G-10M_deeprl.ext3:ro \
+	    --overlay /scratch/work/public/singularity/openmpi4.1.1-ubuntu18.04.sqf:ro \
+	    --overlay /scratch/work/public/singularity/mujoco200-dep-cuda11.1-cudnn8-ubunutu18.04.sqf:ro \
+	    /scratch/work/public/singularity/cuda11.1-cudnn8-devel-ubuntu18.04.sif \
+            /bin/bash -c "
+source /ext3/openmpi.sh
+if [ -f /ext3/env.sh ]; then source /ext3/env.sh; fi
+source ~/.bashrc
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/vm2134/.mujoco/mujoco200/bin
+conda activate hw2_drq_new
+conda info
+eval $args
+#exit
+"
+
+#exit
+
---
+++ Code/slurm_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+#SBATCH --open-mode=append
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=05:00:00
+#SBATCH --mem=100GB
+#SBATCH --job-name=exp_norepeat
+#SBATCH --mail-type=END
+#SBATCH --gres=gpu
+#SBATCH --array=1
+#SBATCH --output=./logs/dqn/%j_%x.out
+#SBATCH --error=./logs/dqn/%j_%x.err
+#SBATCH --export=ALL
+
+bash ./run-mpi4py-singularity.bash \
+	python3 
