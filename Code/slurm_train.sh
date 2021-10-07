9e33975de290c119f9d850ba6adc8c21c7aeba72
vaibhav117 <vaibhavmathur117@gmail.com>
2021-10-07 18:31:02 -0400

    DQN SpaceInvaders

--- Code/slurm_train.sh
+++ Code/slurm_train.sh
@@ -3,14 +3,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=06:00:00
+#SBATCH --time=05:00:00
 #SBATCH --mem=100GB
 #SBATCH --mail-type=END
 #SBATCH --gres=gpu
 #SBATCH --array=1
-#SBATCH --job-name=dqn_pong
-#SBATCH --output=./logs/dqn_pong/%j_%x.out
-#SBATCH --error=./logs/dqn_pong/%j_%x.err
+#SBATCH --job-name=dqn_spaceinvaders
+#SBATCH --output=./logs/dqn_spaceinvaders/%j_%x.out
+#SBATCH --error=./logs/dqn_spaceinvaders/%j_%x.err
 #SBATCH --export=ALL
 
 bash ./run-mpi4py-singularity.bash \
