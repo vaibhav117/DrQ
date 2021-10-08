bb0d0f38c4eae8e1864b1e417fa954f84c2db4eb
vaibhav117 <vaibhavmathur117@gmail.com>
2021-10-07 23:02:58 -0400

    Reduce resources

--- Code/slurm_train.sh
+++ Code/slurm_train.sh
@@ -2,9 +2,9 @@
 #SBATCH --open-mode=append
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=05:00:00
-#SBATCH --mem=100GB
+#SBATCH --mem=50GB
 #SBATCH --mail-type=END
 #SBATCH --gres=gpu
 #SBATCH --array=1
