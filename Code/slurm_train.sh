f3c66dfdb9ade8573423362317ec56820b5b7563
vaibhav117 <vaibhavmathur117@gmail.com>
2021-10-08 02:16:22 -0400

    P_D_DQN Pong

--- Code/slurm_train.sh
+++ Code/slurm_train.sh
@@ -8,9 +8,9 @@
 #SBATCH --mail-type=END
 #SBATCH --gres=gpu
 #SBATCH --array=1
-#SBATCH --job-name=p_d_dqn_breakout
-#SBATCH --output=./logs/p_d_dqn_breakout/%j_%x.out
-#SBATCH --error=./logs/p_d_dqn_breakout/%j_%x.err
+#SBATCH --job-name=p_d_dqn_pong
+#SBATCH --output=./logs/p_d_dqn_pong/%j_%x.out
+#SBATCH --error=./logs/p_d_dqn_pong/%j_%x.err
 #SBATCH --export=ALL
 
 bash ./run-mpi4py-singularity.bash \
